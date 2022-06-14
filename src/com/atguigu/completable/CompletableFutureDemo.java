617f19956d35c2f1ce95755a7dfedd80f5cd1967
Chen <chen@163.com>
2022-06-14 23:01:24 +0800

    异步调用和同步调用

---
+++ src/com/atguigu/completable/CompletableFutureDemo.java
@@ -0,0 +1,36 @@
+package com.atguigu.completable;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ *
+ *
+ * 场景:主线程里面创建一个 CompletableFuture，然后主线程调用 get 方法会阻塞，最后我们在一个子线程中使其终止。
+ * @author ChenCheng
+ * @create 2022-06-14 22:58
+ */
+
+//异步调用和同步调用
+public class CompletableFutureDemo {
+    public static void main(String[] args) throws Exception {
+        //同步调用
+        CompletableFuture<Void> completableFuture1 = CompletableFuture.runAsync(()->{
+            System.out.println(Thread.currentThread().getName()+" : CompletableFuture1");
+        });
+        completableFuture1.get();
+
+        //mq消息队列
+        //异步调用
+        CompletableFuture<Integer> completableFuture2 = CompletableFuture.supplyAsync(()->{
+            System.out.println(Thread.currentThread().getName()+" : CompletableFuture2");
+            //模拟异常
+           // int i = 10/0;
+            return 1024;
+        });
+        completableFuture2.whenComplete((t,u)->{
+            System.out.println("------t="+t);
+            System.out.println("------u="+u);
+        }).get();
+
+    }
+}
